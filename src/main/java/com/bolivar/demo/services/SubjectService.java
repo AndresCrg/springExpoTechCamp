cf1ef2e4ea8d4abfd37252a78b12545f4c2ea9e8
Jhonathan Pérez <jhonathan.perez@segurosbolivar.com>
2022-11-01 18:02:30 -0500

    Add SubjectService logic

--- src/main/java/com/bolivar/demo/services/SubjectService.java
+++ src/main/java/com/bolivar/demo/services/SubjectService.java
@@ -1,13 +1,46 @@
 package com.bolivar.demo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bolivar.demo.models.Student;
+import com.bolivar.demo.models.Subject;
+import com.bolivar.demo.repositories.IStudentRepository;
 import com.bolivar.demo.repositories.ISubjectRepository;
 
 @Service
 public class SubjectService {
 	
 	@Autowired
-	private ISubjectRepository repository;
+	private ISubjectRepository iSubjectRepository;
+	
+
+	//	public StudentService(IStudentRepository iStudentRepository) {
+	//		this.iStudentRepository = iStudentRepository;
+	//	}
+
+	public List<Subject> getAllSubjects() {
+		return iSubjectRepository.findAll();
+	}
+
+	public Subject saveSubject(Subject subject) {
+		return iSubjectRepository.save(subject);
+	}
+
+	public Subject updateSubject(Long id, Subject subject) {
+		Subject oldSubject = iSubjectRepository.findById(id).orElseThrow();
+		oldSubject.setName(subject.getName());
+		oldSubject.setCredits(subject.getCredits());
+	
+		
+		return iSubjectRepository.save(oldSubject);
+		 
+	}
+
+	public void deleteSubject(Long id) {
+		Subject subject = iSubjectRepository.findById(id).orElseThrow();
+		iSubjectRepository.delete(subject);
+	}
 }
